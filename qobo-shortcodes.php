acd3596d6fe84046e9b9b4346b181186178fdeef
Neophytos Constantinou <n.constantinou@qobo.biz>
2015-01-07 11:14:52 +0200

    Initial Commit
    Some useful shortcode functions.
    - qbsc-get-post-field, get field of a post
    - qbsc-get-post-thumbnail, get thumbnail of a post
    - qbsc-get-post-anchor, get anchor linking to a post
    - qbsc-trim, trim text by characters, words, html and addition of suffix text

---
+++ qobo-shortcodes.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Qobo Shortcodes
+Plugin URI: http://www.qobo.biz
+Description: Qobo Shortcodes
+Author: Qobo ltd
+Version: 0.1
+Author URI: http://www.qobo.biz
+*/
+
+define('QBSC__PLUGIN_FILE', __FILE__);
+define('QBSC__PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+require_once (QBSC__PLUGIN_DIR.'qbsc_shortcodes.php');
