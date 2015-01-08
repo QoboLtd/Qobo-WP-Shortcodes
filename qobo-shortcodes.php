26321c20c0a8fb3757a6a0c9f8c4c43360e4776f
Neophytos Constantinou <n.constantinou@qobo.biz>
2015-01-08 09:36:25 +0200

    Changed versioning format

--- qobo-shortcodes.php
+++ qobo-shortcodes.php
@@ -4,10 +4,9 @@ Plugin Name: Qobo Shortcodes
 Plugin URI: http://www.qobo.biz
 Description: Qobo Shortcodes
 Author: Qobo ltd
-Version: 0.1
+Version: 1.0.0
 Author URI: http://www.qobo.biz
 */
 
-define('QBSC__PLUGIN_FILE', __FILE__);
 define('QBSC__PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 require_once (QBSC__PLUGIN_DIR.'qbsc_shortcodes.php');
